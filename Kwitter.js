f047caf52d3ff990d0619b2ab563c23f101b21f8
puputcho <67434177+puputcho@users.noreply.github.com>
2022-08-30 20:33:36 -0300

    Delete Kwitter.js

--- Kwitter.js
+++
@@ -1,5 +0,0 @@
-function addUser() {
-    username = document.getElementById('username').value
-    localStorage.setItem('username', username);
-    window.location.assign("https://puputcho.github.io/XAT/Kwitter_room.html")
-}
